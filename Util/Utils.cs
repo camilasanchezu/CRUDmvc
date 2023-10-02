0cf45a8d6bf4505b254a27b4317e60e432e3772b
Camila Isabel Sánchez <camila.sanchez.urresta@udla.edu.ec>
2023-10-01 22:51:14 -0500

    Agregar archivos de proyecto.

---
+++ Util/Utils.cs
@@ -0,0 +1,33 @@
+﻿using CRUDmvc.Models;
+
+namespace CRUDmvc.Util
+{
+    public class Utils
+    {
+        public static List<Producto> ListaProducto = new List<Producto>()
+        {
+            new Producto()
+            {
+                IdProducto=1,
+                Nombre="Producto1",
+                Descripcion="Descripción1",
+                Cantidad=1
+            },
+            new Producto()
+            {
+                IdProducto = 2,
+                Nombre="Producto2",
+                Descripcion="Descripción2",
+                Cantidad=2
+            },
+            new Producto()
+            {
+                IdProducto=3,
+                Nombre="Producto3",
+                Descripcion="Descripción3",
+                Cantidad=3
+            },
+        }; 
+
+    }
+}
